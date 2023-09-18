c25571b17b53ffe88a5a7d02e972790cce0ccedb
ybsharan <bsharanyadav@gmail.com>
2023-09-18 11:45:31 +0530

    Home page and About Page Added

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,6 +1,39 @@
-
 const About = () => {
-  return <div>About</div>;
+  return (
+    <div className='p-5 m-2 max-w-2xl mx-auto'>
+      <h1 className='font-bold uppercase text-xl text-center my-2'>
+        MERN Stack Development
+      </h1>
+      <p className='text-justify'>
+        The project at hand represents a remarkable example of a Full Stack MERN
+        (MongoDB, Express.js, React, Node.js) application, embodying the
+        principles of modern web development. It&apos;s engineered to facilitate
+        robust communication between the front-end and back-end components
+        through APIs, enabling seamless data exchange and dynamic user
+        experiences.
+      </p>
+      <p className='text-justify my-1'>
+        One of the standout features of this application is its user-friendly
+        sign-in page. Here, the project leverages the power of OAuth
+        authentication, specifically Google Sign-In, to streamline user access.
+        Users can effortlessly log in using their Google accounts, eliminating
+        the need for extensive registration processes and remembering yet
+        another set of login credentials. This approach not only simplifies user
+        onboarding but also enhances security by relying on Google&apos;s robust
+        authentication framework.
+      </p>
+      <p className='text-justify my-1'>
+        Overall, this MERN stack project showcases the synergy of cutting-edge
+        technologies and best practices in web development. It empowers
+        businesses to create feature-rich, responsive, and secure applications
+        while offering users a seamless and intuitive experience, especially
+        through its hassle-free Google Sign-In feature. Whether you&apos;re a
+        developer looking to learn from its architecture or a user exploring its
+        capabilities, this project is a testament to the endless possibilities
+        in modern web application development.
+      </p>
+    </div>
+  );
 };
 
 export default About;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,28 @@
-
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className='p-5 m-2 max-w-2xl mx-auto'>
+      <h1 className='font-bold uppercase text-xl text-center my-2'>
+        MERN Stack Development
+      </h1>
+      <p className='text-justify'>
+        This project exemplifies a Full Stack MERN (MongoDB, Express.js, React,
+        Node.js) application, enabling seamless communication between front-end
+        and back-end components through APIs. It focuses on fundamental CRUD
+        (Create, Read, Update, Delete) operations, ideal for versatile and
+        data-driven applications.
+      </p>
+      <p className='text-justify my-1'>
+        Noteworthy is the user-friendly Google Sign-In feature on the sign-in
+        page, simplifying user access and enhancing security. React powers the
+        front-end, ensuring a responsive UI across devices. Node.js and
+        Express.js form the high-performance back-end, while MongoDB handles
+        data storage. This project showcases modern web development practices,
+        offering businesses feature-rich, secure applications, and users an
+        intuitive experience. It&apos;s a testament to the potential of MERN
+        stack applications in today&apos;s web development landscape.
+      </p>
+    </div>
+  );
 };
 
 export default Home;
